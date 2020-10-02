9d257a9a92e44c375e0d55d9c5267149de6edeae
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-10-02 19:50:45 +0200

    react setup & register page

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Register from './pages/Register';
+import './App.scss';
+
+function App() {
+  return (
+    <Container className="pt-4">
+      <Register />
+    </Container>
+  );
+}
+
+export default App;
