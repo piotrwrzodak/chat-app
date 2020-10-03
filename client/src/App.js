15880304a555da5168de266b356feb23130cd5a1
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-10-03 14:12:22 +0200

    wired register and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import ApolloProvider from './ApolloProvider';
+
 import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
 import './App.scss';
 
 function App() {
   return (
-    <Container className="pt-4">
-      <Register />
-    </Container>
+    <ApolloProvider>
+      <Router>
+        <Container className="pt-4">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,72 +1,49 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
-import { gql, useMutation } from '@apollo/client';
+import { gql, useLazyQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
-const REGISTER_USER = gql`
-  mutation register(
-    $username: String!
-    $email: String!
-    $password: String!
-    $confirmPassword: String!
-  ) {
-    register(
-      username: $username
-      email: $email
-      password: $password
-      confirmPassword: $confirmPassword
-    ) {
+const LOGIN_USER = gql`
+  query login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       username
       email
       createdAt
+      token
     }
   }
 `;
 
-export default function Register(props) {
+export default function Login(props) {
   const [variables, setVariables] = useState({
-    email: '',
     username: '',
     password: '',
-    confirmPassword: '',
   });
   const [errors, setErrors] = useState({});
 
-  const [registerUser, { loading }] = useMutation(REGISTER_USER, {
-    update(_, __) {
-      props.history.push('/login');
-    },
+  const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
     onError(err) {
       //console.log(err.graphQLErrors[0].extensions.errors);
       setErrors(err.graphQLErrors[0].extensions.errors);
     },
+    onCompleted(data) {
+      localStorage.setItem('token', data.login.token);
+      props.history.push('/');
+    },
   });
 
-  const submitRegisterForm = (e) => {
+  const submitLoginForm = (e) => {
     e.preventDefault();
 
-    registerUser({ variables });
+    loginUser({ variables });
     //console.log(variables);
   };
   return (
     <>
       <Row className="bg-white py-5 justify-content-center">
         <Col sm={8} md={6} lg={4}>
-          <h1 className="text-center">Register</h1>
-          <Form onSubmit={submitRegisterForm}>
-            <Form.Group>
-              <Form.Label className={errors.email && 'text-danger'}>
-                {errors.email ?? 'Email address'}
-              </Form.Label>
-              <Form.Control
-                type="email"
-                value={variables.email}
-                className={errors.email && 'is-invalid'}
-                onChange={(e) =>
-                  setVariables({ ...variables, email: e.target.value })
-                }
-              />
-            </Form.Group>
+          <h1 className="text-center">Login</h1>
+          <Form onSubmit={submitLoginForm}>
             <Form.Group>
               <Form.Label className={errors.username && 'text-danger'}>
                 {errors.username ?? 'Username'}
@@ -93,30 +70,14 @@ export default function Register(props) {
                 }
               />
             </Form.Group>
-            <Form.Group>
-              <Form.Label className={errors.confirmPassword && 'text-danger'}>
-                {errors.confirmPassword ?? 'Repeat password'}
-              </Form.Label>
-              <Form.Control
-                type="password"
-                value={variables.confirmPassword}
-                className={errors.confirmPassword && 'is-invalid'}
-                onChange={(e) =>
-                  setVariables({
-                    ...variables,
-                    confirmPassword: e.target.value,
-                  })
-                }
-              />
-            </Form.Group>
 
             <div className="text-center">
               <Button variant="success" type="submit" disabled={loading}>
-                {loading ? 'loading..' : 'Register'}
+                {loading ? 'loading..' : 'Login'}
               </Button>
               <br />
               <small className="pt-4">
-                Already have an account? <Link to="/login">Login</Link>
+                Don't have an account? <Link to="/register">Register</Link>
               </small>
             </div>
           </Form>
