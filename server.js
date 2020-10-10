9fa676ef8c818d4b6be00097b660aff5acca6757
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-10-10 17:49:43 +0200

    prepared code for deployment

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const { ApolloServer } = require('apollo-server');
+
+require('dotenv').config();
 const { sequelize } = require('./models');
 
 // A map of functions which return data for the schema.
@@ -10,10 +12,12 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: contextMiddleware,
+  subscriptions: { path: '/' },
 });
 
-server.listen().then(({ url }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Subscription ready at ${subscriptionsUrl}`);
 
   sequelize
     .authenticate()
